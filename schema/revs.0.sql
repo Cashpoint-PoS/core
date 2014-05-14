4f834f2259476f6e03d5031af6cb89db7b695a75
Marco Schuster <marco+github@m-s-d.eu>
2014-05-14 22:33:04 +0200

    add schema for table revs

---
+++ schema/revs.0.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `revs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `rev` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
