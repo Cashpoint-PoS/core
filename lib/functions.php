6e33369932f0c6b9e526805546de53cdef776c7e
Marco Schuster <marco+github@m-s-d.eu>
2014-05-15 22:47:40 +0200

    lib/functions.php: add getPassword function for shellscripts

--- lib/functions.php
+++ lib/functions.php
@@ -137,3 +137,51 @@ function pretty_json($json) {
     }
     return $result;
 }
+//src http://www.dasprids.de/blog/2008/08/22/getting-a-password-hidden-from-stdin-with-php-cli
+/**
+ * Get a password from the shell.
+ *
+ * This function works on *nix systems only and requires shell_exec and stty.
+ *
+ * @param  boolean $stars Wether or not to output stars for given characters
+ * @return string
+ */
+function getPassword($stars = false)
+{
+		//windows can't do this
+		if(strtolower(substr(PHP_OS,0,3))=="win")
+			return trim(fgets(STDIN), "\n");
+
+    // Get current style
+    $oldStyle = shell_exec('stty -g');
+
+    if ($stars === false) {
+        shell_exec('stty -echo');
+        $password = rtrim(fgets(STDIN), "\n");
+    } else {
+        shell_exec('stty -icanon -echo min 1 time 0');
+
+        $password = '';
+        while (true) {
+            $char = fgetc(STDIN);
+
+            if ($char === "\n") {
+                break;
+            } else if (ord($char) === 127) {
+                if (strlen($password) > 0) {
+                    fwrite(STDOUT, "\x08 \x08");
+                    $password = substr($password, 0, -1);
+                }
+            } else {
+                fwrite(STDOUT, "*");
+                $password .= $char;
+            }
+        }
+    }
+
+    // Reset old style
+    shell_exec('stty ' . $oldStyle);
+
+    // Return the password
+    return $password;
+}
